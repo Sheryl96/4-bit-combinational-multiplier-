59a70c69e2262348fb022831160a5fdbcb596fcb
Sheryl Bernard <sheryl396@gmail.com>
2018-09-25 21:56:34 -0700

    updated full adder

--- full_adder.v
+++ full_adder.v
@@ -2,7 +2,7 @@ module full_adder(a,b,cin,s,cout);
 input a,b,cin;
 output s,cout;
 
-//full adder assignments.
 assign s=(a^b^cin);
-assign cout=((a&b)|(b&cin)|(cin&a));
-endmodule 
+assign cout = ((a&b)|(b&cin)|(cin&a));
+
+endmodule
