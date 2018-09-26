2f202243d6860004d94ad32a32817cbbc67d14ef
Sheryl Bernard <sheryl396@gmail.com>
2018-09-25 21:57:24 -0700

    updated testbench

--- test_file.v
+++ test_file.v
@@ -4,24 +4,33 @@ module test_file();
 reg[3:0] a,b;
 wire[7:0] s;
 
-initial begin
+arr_mul s1(a,b,s);
+
+initial
+begin
+$display("Project by: Sheryl Bernard, Sushmitha Shiva Shankar");
+$display("Test case 1:");
+$monitor("	Result=%8b",s);
 a=4'b0010;
 b=4'b0011;
-$display("      %b",s[0]);
 #10
 
+#10
+$display("Test case 2:");
 a=4'b1010;
 b=4'b0101;
-$display("	%8b",s);
-#10
 
+#10
+$display("Test case 3:");
 a=4'hF;
 b=4'hF;
-#10
 
+#10
+$display("Test case 4:");
 a=4'b1001;
 b=4'b0111;
+#10
+$finish;
+end
 
-end 
-
-endmodule 
+endmodule
